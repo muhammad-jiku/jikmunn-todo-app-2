7e9840ddc2fe1c283ad79050abaca56c221f38e1
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-16 19:09:42 +0600

    displaying single todo data

--- src/app/Components/Todos/SingleTodo.jsx
+++ src/app/Components/Todos/SingleTodo.jsx
@@ -13,8 +13,14 @@ const SingleTodo = async ({ params }) => {
   const todo = await fetchTodo(todoId);
   console.log(todo);
   return (
-    <div>
-      <h1>Todo Id page: {todoId}</h1>
+    <div className="p-10 bg-yellow-200 border-2 m-2 shadow-lg">
+      <p>
+        #{todo.id} : {todo.title}
+      </p>
+      <p>Completed: {todo.completed ? 'Yes' : 'No'} </p>
+      <p className="border-t border-black mt-5 text-right">
+        By User: {todo.userId}
+      </p>
     </div>
   );
 };
