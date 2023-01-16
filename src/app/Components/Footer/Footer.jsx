01558fec696cc59fbf2803346881760730e0b81e
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-16 13:23:54 +0600

    tag names modified

--- src/app/Components/Footer/Footer.jsx
+++ src/app/Components/Footer/Footer.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <div className="mt-auto p-5 bg-gradient-to-tr from-red-500 via-red-700 to-red-900">
+    <footer className="mt-auto p-5 bg-gradient-to-tr from-red-500 via-red-700 to-red-900">
       <p className="text-center text-white">
         &copy; {year} time to code - todos app built with Next.js by Muhammad
         Azizul Hoque Jiku{' '}
       </p>
-    </div>
+    </footer>
   );
 };
 
--- src/app/Components/Navbar/Navbar.jsx
+++ src/app/Components/Navbar/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="p-5 bg-red-800 text-center">
+    <header className="p-5 bg-red-800 text-center">
       <h1 className="text-white text-2xl font-bold">Welcome here!</h1>
-    </div>
+    </header>
   );
 };
 
