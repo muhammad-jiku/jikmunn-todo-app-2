ecf0e9ca128efd49cbb19ac7393a2e4df0c49d45
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-17 14:25:26 +0600

    react suspense initialized

--- src/app/Components/Todos/TodoList.jsx
+++ src/app/Components/Todos/TodoList.jsx
@@ -2,6 +2,10 @@ import Link from 'next/link';
 import React from 'react';
 
 const fetchTodos = async () => {
+  // timeout for random number of seconds between 1 and 5
+  const timeout = Math.floor(Math.random() * 5 + 1) * 1000;
+  await new Promise((resolve) => setTimeout(resolve, timeout));
+
   const res = await fetch('https://jsonplaceholder.typicode.com/todos');
   const todos = res.json();
   return todos;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,29 @@
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from './page.module.css';
+import TodoList from './Components/Todos/TodoList';
+import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-5xl font-bold text-red-800 underline">
-        Hello world!
-      </h1>
+    <div className="flex flex-col m-5 justify-center">
+      <Suspense fallback={<p className="text-red-800">reloading todos..</p>}>
+        <h1 className="text-3xl font-bold">Loading Todos</h1>
+        <div className="flex space-x-2">
+          <TodoList />
+        </div>
+      </Suspense>
+
+      <Suspense
+        fallback={<p className="text-green-800">reloading more todos..</p>}
+      >
+        <h1 className="text-xl font-extrabold">Loading more todos</h1>
+        <div className="flex space-x-2">
+          <TodoList />
+        </div>
+      </Suspense>
     </div>
   );
 }
