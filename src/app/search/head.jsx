0c94c0aef1e3ad9b4093389623e06194d73d67bd
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-17 13:40:54 +0600

    dynamic head added

---
+++ src/app/search/head.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Head = () => {
+  return (
+    <>
+      <title>Todo App: Search Results</title>
+    </>
+  );
+};
+
+export default Head;
---
+++ src/app/todos/head.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Head = () => {
+  return (
+    <>
+      <title>Todo lists</title>
+    </>
+  );
+};
+
+export default Head;
